3f482a6daec3bfae900d187e1e0b3cb2a62841a1
Ulyana <ubrataus@gmail.com>
2022-07-10 20:06:48 +0300

    feat: add skeleton component, create routes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import "./scss/app.scss";
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
 
-import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+import "./scss/app.scss";
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  /* we put function into useEffect to prevent it's multiple rerenders */
-  useEffect(() => {
-    fetch("https://62c6b6ff74e1381c0a66e8cb.mockapi.io/items")
-      .then(res => res.json())
-      .then(arr => {
-        setItems(arr);
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            {/* <Categories /> */}
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map(item => (
-              <PizzaBlock
-                key={item.id} /* or {... obj} with spread */
-                title={item.title}
-                price={item.price}
-                image={item.imageUrl}
-                sizes={item.sizes}
-                types={item.types}
-              />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  /* we put function into useEffect to prevent it's multiple rerenders */
+  useEffect(() => {
+    fetch("https://62c6b6ff74e1381c0a66e8cb.mockapi.io/items") /* async req */
+      .then(res => res.json())
+      .then(arr => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map(item => (
+              <PizzaBlock
+                key={item.id}
+                title={item.title} /* or {... obj} with spread */
+                price={item.price}
+                image={item.imageUrl}
+                sizes={item.sizes}
+                types={item.types}
+              />
+            ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
